b505ed77fdd470dc6d265d5ec573001ac63eb090
abhin-dynamify <abhinav.priyadarshi@dynamify.com>
2024-07-16 23:15:41 +0530

    fix: cleanup.

--- src/ios/Adyen.swift
+++ src/ios/Adyen.swift
@@ -2,9 +2,6 @@ import Adyen
 import PassKit
 @objc(Adyen) class Adyen: CDVPlugin, InitialDataFlowProtocolV1 {
     
-    
-    //    private var dropInExample: DropInExample?
-    
     private var session: AdyenSession?
     private var clientKey: String = ""
     private var sessionId: String = ""
@@ -12,21 +9,9 @@ import PassKit
     private var context: AdyenContext?
     private var dropInComponent: DropInComponent?
     private var callbackId: String = ""
-    
-    
-    internal weak var presenter: PresenterExampleProtocol?
-    
-    //    @objc(pluginInitialize)
-    //      override func pluginInitialize() {
-    //        DispatchQueue.global().async {
-    //            self.dropInExample = DropInExample()
-    //        }
-    //      }
-    
-    
+        
     @objc(requestCharge:)
     func requestCharge(command: CDVInvokedUrlCommand) {
-        //        dropInExample?.start()
         guard let paymentRequest = command.argument(at: 0) as? [String: Any],
               let amountDict = paymentRequest["amount"] as? [String: Any],
               let currency = amountDict["currency"] as? String,
@@ -41,45 +26,24 @@ import PassKit
               let mode = paymentRequest["mode"] as? String,
               let sessionData = paymentRequest["sessionData"] as? String,
               let clientKey = paymentRequest["clientKey"] as? String else {
-            // Handle invalid arguments error
-            //            let pluginResult = CDVPluginResult(status: .error, messageAs: "Invalid payment request arguments")
-            //            self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
             sendErrorResult(resultCode: "Error", callbackId: self.callbackId)
             return
         }
         
-        AdyenLogging.isEnabled = true
+//        AdyenLogging.isEnabled = true
         self.callbackId = command.callbackId
         self.context = generateContext(clientKey: clientKey, currencyCode: currency, countryCode: countryCode, value: value)
         self.sessionId = id
         self.SessionData = sessionData
         self.clientKey = clientKey
         
-        print("Session id ")
-        print(self.sessionId)
-        print("______")
-        print("session data")
-        print(self.SessionData)
-        print("client key")
-        print(self.clientKey)
-        
-        
         loadSession { [weak self] response in
             guard let self else { return }
-            print("here 2")
-            
-            
-            print("here 3")
-            
             switch response {
-                
             case let .success(session):
-                print("here 4")
                 self.session = session
                 self.presentComponent(with: session)
-                
             case let .failure(error):
-                print("here 5")
                 self.presentAlert(with: error)
             }
         }
@@ -88,7 +52,6 @@ import PassKit
     
     
     func presentt(viewController: UIViewController, completion: (() -> Void)?) {
-        print("here 21")
         // Ensure there is a visible view controller to present from
         guard let currentViewController = UIApplication.shared.keyWindow?.rootViewController else {
             fatalError("No root view controller available to present from.")
@@ -105,13 +68,9 @@ import PassKit
     }
     
     private func presentComponent(with session: AdyenSession) {
-        print("here 17")
         let dropIn = dropInComponent(from: session)
         presentt(viewController: dropIn.viewController, completion: nil)
-        print("here 18")
         dropInComponent = dropIn
-        print("here 19")
-        
     }
     
     private func initializeApplePay() -> DropInComponent.Configuration {
@@ -130,30 +89,23 @@ import PassKit
             dropInConfiguration.applePay = .init(payment: applePayPayment, merchantIdentifier: merchantIdentifier)
             return dropInConfiguration
         } catch {
-            print("Error initializing apple pay")
             return DropInComponent.Configuration()
         }
     }
     
     private func dropInComponent(from session: AdyenSession) -> DropInComponent {
-        print("here 13")
         let paymentMethods = session.sessionContext.paymentMethods
         let dropInConfiguration = initializeApplePay()
         let component = DropInComponent(paymentMethods: paymentMethods,
                                         context: self.context!,
                                         configuration: dropInConfiguration,
                                         title:"app")
-        print("here 14")
         component.delegate = session
-        print("here 15")
         component.partialPaymentDelegate = session
-        print("here 16")
-        
         return component
     }
     
     private func presentAlert(with error: Error, retryHandler: (() -> Void)? = nil) {
-        print("here 11")
         print(error.localizedDescription)
         let errorMessage = error.localizedDescription
         
@@ -181,28 +133,18 @@ import PassKit
     
     
     private func loadSession(completion: @escaping (Result<AdyenSession, Error>) -> Void) {
-        print ("here 6")
         requestAdyenSessionConfiguration (sessionId: self.sessionId, sessionData: self.SessionData, context: self.context!){ [weak self] response in
-            print ("here 7")
             guard let self else { return }
-            
-            print ("here 8")
             switch response {
             case let .success(config):
                 AdyenSession.initialize(with: config,
                                         delegate: self,
                                         presentationDelegate: self,
                                         completion: completion)
-                print ("here 9")
             case let .failure(error):
                 completion(.failure(error))
-                print ("here 10")
             }
         }
-    }
-    
-    func getSessionsData() -> String {
-        return "Ab02b4c0!BQABAgBOAjAoXzGZQQNwdB7D17XRULITLO/R1Nmz3nIbM+Llx89byTgpUFQUA0CFAv4+XG5zHCn6BbN4vw2twTKqanToYZqJqaKQ9pX4Lw6DhbwZo3U1DIiHlx53lIJwb4Vuec/u1BCtU8SvroBFqah2eXHpksVLIYxrWtgcnokUxVUy4kbysJ9lC6oSVEft7vo1I31c+IPQSrn/7UlkjAXQ4lde//PQVsPQv2qnAPUBY1O9Qc9wVAQuUgQPrXVnbbHXyJ6hrciQSXdtabg1OCBFLGulTdcId3t1pKGvbispG66JiarsT9fRBkg+QUUBcDoO/scc4AXaklNQNcvoV8TjWxh6wxaINszZDWM7+r35G6gmGJx4vgfN/OrmFNKRbaYfns6d2AG13Uk6UrY7Is2xkuGjG9eQ5BvV3f4BIrD6BI0w609g321DZfI3BSW+ZM4sRSv77ay6MwWwdLx4qGXeUSn4hHLEyhIlVTkat5vvMDrxo4uxpovxZuhiVK+rhDRTo9M55RqikpMsGvzr8/nYv2Bqw+6C1/tA6hb/FRb+GQ/FYw3oxKCHpH/ljnKugYNI1pD6JQyGKP8bjFDk5Kq9qWSmybj7q9+1ytllP2cr+GXewA+cx+ujyGfmQOJqIUbce579fc5L1Zt+pC1BZkX7/0rJf5vyiB3QUMkBCs7XmdEVIwwl9u+Ei9WOi2a4Z9IASnsia2V5IjoiQUYwQUFBMTAzQ0E1MzdFQUVEODdDMjRERDUzOTA5QjgwQTc4QTkyM0UzODIzRDY4REFDQzk0QjlGRjgzMDVEQyJ9GzeLxTDQz79iItZA1WVEQwDvjL9XW/usyanRwcwGbbCtv5mtQL6ELibxoRQKXoBib44VbycsIxEBPQRrtensDuBwkelBNthvi9yHNfO4/KSRsW1AsxBdw9HXOItbrtY/iO9d0YIIbZsC8yHy4/EY2yycUjkGe9E45F9ycfkJCBEdRkmpkMyLLKf6RI61qnUgDB6sPL4x6SPE4EzrxENJEXOUS+nYb7lu4dfJofwSciTAh/feoM1nb86NSZ4UCbJ+HCEdTGugz89KE43VDipApsAFDh/RHvO95Sn+X6i1hptAV1+9BSv0DhEa84fejawwkgnE7Er6xkcJHWsQpjhEfd2gxqg9qSA2raip53X0Un4qBcjXuWdSYipuWd4oiZhOw8vznvxqYGDvKV/f1bOu9Dj0hGeGk6kln/0ouTX7Cmsa1fmKwlQD7QDnwNDFLc790VEc5KEhdjzK2BUEk17OEkDbHdmnwj+srkxFBGTdk2I63Bgmavicy8C89icxvY01WlfX5dWH3wYcyBU5zXqlLqllo4PUgP/WGXc5aGEicqfm1Qv/v8UQteZhn+ROaFkiiyFFYoJHEWSwwb1I6nBttjpsHjJzEA3NTQogBFwbBHqlAWndfOnvOEDL2dDy4TuuBcmJ"
     }
     
     func presentAlert(withTitle title: String, message: String?) {
@@ -247,12 +189,7 @@ import PassKit
         }
     }
     
-    func getSessionId() -> String {
-        return "CSEF83B7707477A224"
-    }
-    
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
-        print("something happened")
         return RedirectComponent.applicationDidOpen(from: url)
     }
     
@@ -277,7 +214,7 @@ import PassKit
     
     func handlePluginResult(resultCode: String, callbackId: String) {
         switch resultCode {
-        case "Authorised", "Received", "Pending", "PresentToShopper":
+        case "Authorised", "Received", "Pending", "PresentToShopper", "Cancelled":
             self.sendSuccessResult(resultCode: resultCode, callbackId: callbackId)
         default:
             self.sendErrorResult(resultCode: resultCode, callbackId: callbackId)
@@ -332,7 +269,6 @@ extension Adyen: AdyenSessionDelegate {
         default:
             dismissAndShowAlert(withTitle: "Error", message: error.localizedDescription, resultCode: "Error")
         }
-        //        dismissAndShowAlert(false, error.localizedDescription)
     }
     
     func didOpenExternalApplication(component: ActionComponent, session: AdyenSession) {}
@@ -341,7 +277,6 @@ extension Adyen: AdyenSessionDelegate {
 
 extension Adyen: PresentationDelegate {
     internal func present(component: PresentableComponent) {
-        print("here 20")
         // The implementation of this delegate method is not needed when using AdyenSession as the session handles the presentation
     }
 }
