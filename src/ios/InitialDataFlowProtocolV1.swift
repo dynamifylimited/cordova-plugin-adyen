59f1c85f27c5a1d32f48ad2bee22bb7062091bb7
Nicholas Huang <nicholas.huang@dynamify.com>
2026-02-05 15:32:31 +0000

    maps Germany

--- src/ios/InitialDataFlowProtocolV1.swift
+++ src/ios/InitialDataFlowProtocolV1.swift
@@ -20,6 +20,7 @@ extension InitialDataFlowProtocolV1 {
     private var environmentMapping: [String: Environment] {
         return [
             "AU": .liveAustralia,
+            "DE": .liveEurope,
             "US": .liveUnitedStates
             // Add more mappings as needed
         ]
